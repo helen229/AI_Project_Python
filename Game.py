08f10050bb30c6b05d21844b89569bf498e0bcd9
YR <renyming@gmail.com>
2019-02-28 22:12:09 -0500

    Implementation of State class and generating

--- Game.py
+++ Game.py
@@ -50,7 +50,7 @@ class Game:
 
     def place_card(self, card_type, x, y):
 
-        is_valid_play, win_list = self.play_normal(card_type, x, y)
+        is_valid_play, win_list, self.prev_card = self.play_normal(card_type, x, y, self.board, self.prev_card, True)
 
         if is_valid_play:
             self.step += 1
@@ -71,7 +71,7 @@ class Game:
     def recycle_card(self, old_x1, old_y1, old_x2, old_y2, new_card_type, new_x, new_y):
         old_seg = self.board[old_y1][old_x1]
 
-        is_valid_play, win_list = self.play_recycle(old_x1, old_y1, old_x2, old_y2, new_card_type, new_x, new_y)
+        is_valid_play, win_list, self.prev_card = self.play_recycle(old_x1, old_y1, old_x2, old_y2, new_card_type, new_x, new_y, self.board, self.prev_card, True)
 
         if is_valid_play:
             self.step += 1
@@ -95,129 +95,135 @@ class Game:
     def get_card(self, x, y):
         return self.board[y][x].parent
 
-    def play_normal(self, card_type, x, y):
+    @staticmethod
+    def play_normal(card_type, x, y, board, prev_card, is_game_play):
         card = Card(x, y, card_type)
         if not card.is_valid():
-            return False, None
-        if not self.valid_position(card):
-            return False, None
+            return False, None, prev_card
+        if not Game.valid_position(card, board):
+            return False, None, prev_card
 
-        win_list = self.validate_win(card)
-        self.prev_card = card
-        return True, win_list
+        if is_game_play:
+            win_list = Game.validate_win(card, board)
+            return True, win_list, card
+        else:
+            return True, None, card
 
-    def play_recycle(self, old_x1, old_y1, old_x2, old_y2, new_card_type, new_x, new_y):
+    def play_recycle(self, old_x1, old_y1, old_x2, old_y2, new_card_type, new_x, new_y, board, prev_card, is_game_play):
 
-        selected_seg1 = self.board[old_y1][old_x1]
-        selected_seg2 = self.board[old_y2][old_x2]
+        selected_seg1 = board[old_y1][old_x1]
+        selected_seg2 = board[old_y2][old_x2]
 
         # check existence, check if the same card, check if the previous card
         if isinstance(selected_seg1, CardSegment) and isinstance(selected_seg2, CardSegment) and \
-                selected_seg1.parent == selected_seg2.parent and selected_seg1.parent != self.prev_card:
+                selected_seg1.parent == selected_seg2.parent and selected_seg1.parent != prev_card:
 
             # if new position same as previous one, and the rotation is the same
             if selected_seg1.parent.seg[0].x == new_x and selected_seg1.parent.seg[0].y == new_y and \
                     selected_seg1.parent.card_type == new_card_type:
-                return False, None
+                return False, None, prev_card
 
             # if remove legal
-            if self.is_remove_legal(selected_seg1.parent):
+            if self.is_remove_legal(selected_seg1.parent, board):
                 # temporarily delete the card
-                self.board[old_y1][old_x1] = 0
-                self.board[old_y2][old_x2] = 0
+                board[old_y1][old_x1] = 0
+                board[old_y2][old_x2] = 0
                 # delegate to play_normal
-                is_valid_play, win_list = self.play_normal(new_card_type, new_x, new_y)
+                is_valid_play, win_list, prev_card = self.play_normal(new_card_type, new_x, new_y, board, prev_card, is_game_play)
                 # restore if it's not a valid new card
                 if not is_valid_play:
-                    self.board[old_y1][old_x1] = selected_seg1
-                    self.board[old_y2][old_x2] = selected_seg2
-                return is_valid_play, win_list
-        return False, None
+                    board[old_y1][old_x1] = selected_seg1
+                    board[old_y2][old_x2] = selected_seg2
+                return is_valid_play, win_list, prev_card
+        return False, None, prev_card
 
-    def is_remove_legal(self, card):
+    def is_remove_legal(self, card, board):
         if card.card_type % 2 == 1:
             # horizontal cards
             if card.seg[0].y < self.max_y - 1:
                 # not the last row
                 for seg in card.seg:
-                    if isinstance(self.board[seg.y + 1][seg.x], CardSegment):
+                    if isinstance(board[seg.y + 1][seg.x], CardSegment):
                         return False
 
             return True
         else:
             if card.seg[1].y < self.max_y - 1:
-                if isinstance(self.board[card.seg[1].y + 1][card.seg[1].x], CardSegment):
+                if isinstance(board[card.seg[1].y + 1][card.seg[1].x], CardSegment):
                     return False
             return True
 
-    def valid_position(self, card):
+    @staticmethod
+    def valid_position(card, board):
         # if positions empty
         for seg in card.seg:
-            if isinstance(self.board[seg.y][seg.x], CardSegment):
+            if isinstance(board[seg.y][seg.x], CardSegment):
                 return False
 
         # temporarily set positions
         for seg in card.seg:
-            self.board[seg.y][seg.x] = seg
+            board[seg.y][seg.x] = seg
 
         # check surrounding cells
         if card.seg[0].y != 0:
-            if (not isinstance(self.board[card.seg[0].y - 1][card.seg[0].x], CardSegment)) or \
-                    (not isinstance(self.board[card.seg[1].y - 1][card.seg[1].x], CardSegment)):
+            if (not isinstance(board[card.seg[0].y - 1][card.seg[0].x], CardSegment)) or \
+                    (not isinstance(board[card.seg[1].y - 1][card.seg[1].x], CardSegment)):
                 for seg in card.seg:
-                    self.board[seg.y][seg.x] = 0
+                    board[seg.y][seg.x] = 0
                 return False
 
         return True
 
-    def validate_win(self, card):
+    @staticmethod
+    def validate_win(card, board):
         win_list = []
 
         for seg in card.seg:
             for mode in Choice:
                 # up
-                count1 = self.validate_win_helper(seg, 0, 1, mode)
+                count1 = Game.validate_win_helper(seg, 0, 1, mode, board)
                 # bottom
-                count2 = self.validate_win_helper(seg, 0, -1, mode)
+                count2 = Game.validate_win_helper(seg, 0, -1, mode, board)
                 if count1 + count2 >= 3:
                     win_list.append([seg, mode])
 
                 # up right
-                count1 = self.validate_win_helper(seg, 1, 1, mode)
+                count1 = Game.validate_win_helper(seg, 1, 1, mode, board)
                 # bottom left
-                count2 = self.validate_win_helper(seg, -1, -1, mode)
+                count2 = Game.validate_win_helper(seg, -1, -1, mode, board)
                 if count1 + count2 >= 3:
                     win_list.append([seg, mode])
 
                 # right
-                count1 = self.validate_win_helper(seg, 1, 0, mode)
+                count1 = Game.validate_win_helper(seg, 1, 0, mode, board)
                 # left
-                count2 = self.validate_win_helper(seg, -1, 0, mode)
+                count2 = Game.validate_win_helper(seg, -1, 0, mode, board)
                 if count1 + count2 >= 3:
                     win_list.append([seg, mode])
 
                 # bottom right
-                count1 = self.validate_win_helper(seg, 1, -1, mode)
+                count1 = Game.validate_win_helper(seg, 1, -1, mode, board)
                 # top left
-                count2 = self.validate_win_helper(seg, -1, 1, mode)
+                count2 = Game.validate_win_helper(seg, -1, 1, mode, board)
                 if count1 + count2 >= 3:
                     win_list.append([seg, mode])
 
         return win_list
 
-    def validate_win_helper(self, seg, increment_x, increment_y, mode):
+    @staticmethod
+    def validate_win_helper(seg, increment_x, increment_y, mode, board):
         x = seg.x
         y = seg.y
         next_x = x + increment_x
         next_y = y + increment_y
 
-        if 0 <= next_x < self.max_x and 0 <= next_y < self.max_y:
-            next_seg = self.board[next_y][next_x]
+        if 0 <= next_x < Game.max_x and 0 <= next_y < Game.max_y:
+            next_seg = board[next_y][next_x]
             if not isinstance(next_seg, CardSegment):
                 return 0
             if (mode == Choice.COLOR and next_seg.color == seg.color) or (
                     mode == Choice.DOT and next_seg.dot == seg.dot):
-                count = self.validate_win_helper(next_seg, increment_x, increment_y, mode) + 1
+                count = Game.validate_win_helper(next_seg, increment_x, increment_y, mode, board) + 1
             else:
                 return 0
         else:
@@ -225,11 +231,12 @@ class Game:
 
         return count
 
-    def print_board(self):
+    @staticmethod
+    def print_board(board):
         for i in range(11, -1, -1):
             print('%3s' % str(i + 1) + ': ', end='')
             for j in range(0, 8):
-                seg = self.board[i][j]
+                seg = board[i][j]
                 fore = Fore.BLACK
                 back = Back.RESET
                 if isinstance(seg, CardSegment):
@@ -258,33 +265,33 @@ Regular Move
 
     print(game.place_card(1, 0, 1))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     print(game.place_card(1, 2, 1))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     print(game.place_card(2, 2, 0))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     # invalid placement
     print(game.place_card(1, 3, 2))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     # invalid placement
     print(game.place_card(1, 0, 0))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     print(game.place_card(1, 0, 2))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     print(game.place_card(7, 0, 3))
     print("Player " + str(game.get_player()))
-    game.print_board()
+    Game.print_board(game.board)
 
     print('''
 ===============================
@@ -293,45 +300,49 @@ Test Recycling
     ''')
     game = Game(Choice.COLOR)
     card_type = [1, 5, 3, 7, 1, 5]
-    for i in range(0, 6):
+    for i in range(6):
         for j in range(0, 7, 2):
             print(game.place_card(card_type[i], j, i))
 
-    game.print_board()
+    Game.print_board(game.board)
 
     # invalid
     print(game.recycle_card(6, 5, 7, 5, 2, 0, 6))
-    game.print_board()
+    Game.print_board(game.board)
 
     print(game.recycle_card(4, 5, 5, 5, 2, 0, 6))
-    game.print_board()
+    Game.print_board(game.board)
 
-    # test deep copy
-    new_board = np.copy(game.board)
-    game.board[5][7].color = Color.RED
+    print('''
+    ===============================
+    Test Recycling
+    ===============================
+        ''')
+    # new_board = np.copy(game.board)
+    # game.board[5][7].color = Color.RED
+    #
+    # print("Original Board")
+    # game.print_board()
+    #
+    # print("Copied Board")
+    # print(new_board[5][7])
 
-    print("Original Board")
-    game.print_board()
-
-    print("Copied Board")
-    print(new_board[5][7])
-
-    # test deep copy speed
-    time0 = time.time()
-    for i in range(10000):
-        tmp = np.copy(game.board)
-        card=Card(0,1,8)
-    time1 = time.time()
-
-    print('np copy: {:f}'.format(time1 - time0))
-
-    time0 = time.time()
-    for i in range(10000):
-        tmp = copy.copy(game.board)
-        card = Card(0, 1, 8)
-    time1 = time.time()
-
-    print('copy copy: {:f}'.format(time1 - time0))
+    # # test deep copy speed
+    # time0 = time.time()
+    # for i in range(10000):
+    #     tmp = np.copy(game.board)
+    #     card=Card(0,1,8)
+    # time1 = time.time()
+    #
+    # print('np copy: {:f}'.format(time1 - time0))
+    #
+    # time0 = time.time()
+    # for i in range(10000):
+    #     tmp = copy.copy(game.board)
+    #     card = Card(0, 1, 8)
+    # time1 = time.time()
+    #
+    # print('copy copy: {:f}'.format(time1 - time0))
 
     # time0 = time.time()
     # for i in range(10000):
--- State.py
+++ State.py
@@ -1,25 +1,111 @@
 import numpy as np
+from CardSegment import CardSegment
+from Game import Game, Choice
 
 
 class State:
 
-    def __init__(self, parent, old_board, old_step, prev_card):
+    def __init__(self, parent, board, step, prev_card):
         """
         Ctor for State class
         :param parent: parent State, can be None for the root node
-        :param old_board: based on which board create the board of current state, usually should be parent.board
-        :param old_step: the count of steps of the previous move
+        :param board: based on which board create the board of current state
+        :param step: the count of steps of the this State
         :param prev_card: the most recent card has been placed in the previous step
         """
-        self.board = np.copy(old_board)
-        self.step = old_step + 1
+        self.board = np.copy(board)
+        self.step = step
         self.prev_card = prev_card
         self.parent = parent
         self.children = []
+        self.highest_row = [-1] * 8
 
-    def add_child(self, child):
+    def generate_children(self):
         """
-        Add a child or children list to current State node
-        :param child: A single State object or a list of State objects
+        Generate children state for a state node and add it the current node's children list
         """
-        self.children.append(child)
+        # TODO: Implement state generation for recycle phase
+        if self.step <= 24:
+            self.generate_regular_move()
+
+    def generate_regular_move(self):
+        if self.highest_row[0] == -1:
+            self.find_highest_row()
+
+        for i in range(8):
+            if self.highest_row[i] < 12:
+                for t in range(1, 9):
+                    new_board = np.copy(self.board)
+                    is_valid_play, win_list, prev_card = Game.play_normal(t, i, self.highest_row[i], new_board,
+                                                                          self.prev_card, False)
+                    if is_valid_play:
+                        state = State(self, new_board, self.step + 1, prev_card)
+                        self.children.append(state)
+
+    def find_highest_row(self):
+        for j in range(8):
+            for i in range(12):
+                if not isinstance(self.board[i][j], CardSegment):
+                    self.highest_row[j] = i
+                    break
+            if self.highest_row[j] == -1:
+                self.highest_row[j] = i
+
+
+def main():
+    # print('''
+    # ===============================
+    # Regular Play: full first row
+    # ===============================
+    #         ''')
+    #
+    # game = Game(Choice.COLOR)
+    # game.place_card(1, 0, 0)
+    # game.place_card(1, 2, 0)
+    # game.place_card(1, 4, 0)
+    # is_valid, prev_card, step, win= game.place_card(1, 6, 0)
+    # Game.print_board(game.board)
+    # state=State(None, game.board, 4, prev_card)
+    # state.generate_children()
+    #
+    # for child in state.children:
+    #     Game.print_board(child.board)
+
+    # print('''
+    # ===============================
+    # Regular Play: empty first row
+    # ===============================
+    #         ''')
+    #
+    # game = Game(Choice.COLOR)
+    # Game.print_board(game.board)
+    # state=State(None, game.board, 0, None)
+    # state.generate_children()
+    #
+    # for child in state.children:
+    #     Game.print_board(child.board)
+
+    print('''
+    ===============================
+    Regular Play: uneven last row
+    ===============================
+            ''')
+
+    game = Game(Choice.COLOR)
+    game.place_card(1, 0, 0)
+    game.place_card(1, 2, 0)
+    game.place_card(1, 4, 0)
+    game.place_card(1, 6, 0)
+    game.place_card(2, 0, 1)
+    game.place_card(1, 2, 1)
+    is_valid, prev_card, step, win= game.place_card(1, 4, 1)
+    Game.print_board(game.board)
+    state=State(None, game.board, 4, prev_card)
+    state.generate_children()
+
+    for child in state.children:
+        Game.print_board(child.board)
+
+
+if __name__ == '__main__':
+    main()
